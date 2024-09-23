8813e770a3a2a01ad7b27f4cec0f86d5bb503f4b
Scott-fo <scottfo@live.com>
2024-09-23 23:00:16 +0100

    untidy, working

--- micrograd/src/engine.rs
+++ micrograd/src/engine.rs
@@ -1,11 +1,4 @@
-use core::panic;
-use std::{
-    cell::RefCell,
-    collections::HashSet,
-    fmt::Display,
-    ops::{Add, Mul},
-    rc::Rc,
-};
+use std::{cell::RefCell, collections::HashSet, fmt::Display, rc::Rc};
 
 #[derive(Clone)]
 pub enum Op {
@@ -16,6 +9,7 @@ pub enum Op {
 
 #[derive(Clone)]
 pub struct Tensor {
+    pub label: String,
     pub data: f64,
     pub grad: RefCell<f64>,
     pub op: Option<Op>,
@@ -24,102 +18,120 @@ pub struct Tensor {
 
 impl Display for Tensor {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Tensor(data={}, grad={})", self.data, self.grad.borrow())
+        write!(
+            f,
+            "Tensor(label={}, data={}, grad={})",
+            self.label,
+            self.data,
+            self.grad.borrow()
+        )
     }
 }
 
 impl Tensor {
-    pub fn new(data: f64) -> Self {
-        Self {
+    pub fn new(data: f64, label: &str) -> Rc<Tensor> {
+        Rc::new(Self {
+            label: label.to_string(),
             data,
             grad: RefCell::new(0.0),
             prev: vec![],
             op: None,
-        }
+        })
     }
 
-    pub fn backward(self: &Self) {
-        if let Some(op) = &self.op {
-            let current_grad = *self.grad.borrow();
+    pub fn add(t1: &Rc<Tensor>, t2: &Rc<Tensor>) -> Rc<Tensor> {
+        let label = format!("{}+{}", t1.label, t2.label);
 
-            match op {
-                Op::Add => {
-                    if self.prev.len() != 2 {
-                        panic!("only handling length of 2 for now");
-                    }
+        Rc::new(Tensor {
+            label,
+            data: t1.data + t2.data,
+            grad: RefCell::new(0.0),
+            op: Some(Op::Add),
+            prev: vec![Rc::clone(t1), Rc::clone(t2)],
+        })
+    }
 
-                    *self.prev[0].grad.borrow_mut() = current_grad * 1.0;
-                    *self.prev[1].grad.borrow_mut() = current_grad * 1.0;
+    pub fn mul(t1: &Rc<Tensor>, t2: &Rc<Tensor>) -> Rc<Tensor> {
+        let label = format!("{}*{}", t1.label, t2.label);
+
+        Rc::new(Tensor {
+            label,
+            data: t1.data * t2.data,
+            grad: RefCell::new(0.0),
+            op: Some(Op::Mul),
+            prev: vec![Rc::clone(t1), Rc::clone(t2)],
+        })
+    }
+
+    pub fn tanh(t: &Rc<Tensor>) -> Rc<Tensor> {
+        let label = format!("tanh({})", t.label);
+
+        Rc::new(Tensor {
+            label,
+            data: t.data.tanh(),
+            grad: RefCell::new(0.0),
+            op: Some(Op::Tanh),
+            prev: vec![Rc::clone(t)],
+        })
+    }
+
+    pub fn backward(v: &Rc<Tensor>) {
+        let mut topo: Vec<Rc<Tensor>> = Vec::new();
+        let mut visited: HashSet<*const Tensor> = HashSet::new();
+
+        fn build_topo(
+            v: &Rc<Tensor>,
+            topo: &mut Vec<Rc<Tensor>>,
+            visited: &mut HashSet<*const Tensor>,
+        ) {
+            if !visited.contains(&(Rc::as_ptr(v) as *const Tensor)) {
+                visited.insert(Rc::as_ptr(v) as *const Tensor);
+                for child in &v.prev {
+                    build_topo(child, topo, visited);
                 }
-                Op::Mul => {
-                    if self.prev.len() != 2 {
-                        panic!("only handling length of 2 for now");
-                    }
+                topo.push(Rc::clone(v));
+            }
+        }
 
-                    *self.prev[0].grad.borrow_mut() = self.prev[1].data * current_grad;
-                    *self.prev[1].grad.borrow_mut() = self.prev[0].data * current_grad;
-                }
-                Op::Tanh => {
-                    if self.prev.len() != 1 {
-                        panic!("expected tanh to only have 1 prev");
-                    }
+        build_topo(v, &mut topo, &mut visited);
 
-                    let g = (1.0 - (self.data * self.data)) * current_grad;
-                    *self.prev[0].grad.borrow_mut() = g;
+        *v.grad.borrow_mut() = 1.0;
+
+        for v in topo.iter().rev() {
+            if let Some(op) = &v.op {
+                let current_grad = *v.grad.borrow();
+                match op {
+                    Op::Add => {
+                        if v.prev.len() != 2 {
+                            panic!("only handling length of 2 for now");
+                        }
+                        *v.prev[0].grad.borrow_mut() += current_grad * 1.0;
+                        *v.prev[1].grad.borrow_mut() += current_grad * 1.0;
+                    }
+                    Op::Mul => {
+                        if v.prev.len() != 2 {
+                            panic!("only handling length of 2 for now");
+                        }
+                        *v.prev[0].grad.borrow_mut() += v.prev[1].data * current_grad;
+                        *v.prev[1].grad.borrow_mut() += v.prev[0].data * current_grad;
+                    }
+                    Op::Tanh => {
+                        if v.prev.len() != 1 {
+                            panic!("expected tanh to only have 1 prev");
+                        }
+                        let g = (1.0 - (v.data * v.data)) * current_grad;
+                        *v.prev[0].grad.borrow_mut() += g;
+                    }
                 }
             }
         }
     }
-
-    pub fn tanh(self: Self) -> Self {
-        let th = self.data.tanh();
-
-        Self {
-            data: th,
-            grad: RefCell::new(0.0),
-            op: Some(Op::Tanh),
-            prev: vec![Rc::new(self)],
-        }
-    }
-
-    pub fn prev_to_string(&self) -> String {
-        let inner = self
-            .prev
-            .iter()
-            .map(|tensor| tensor.to_string())
-            .collect::<Vec<_>>()
-            .join(", ");
-
-        format!("[{}]", inner)
-    }
 }
 
-impl Add for Tensor {
-    type Output = Self;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        let sum = self.data + rhs.data;
-
-        Self {
-            data: sum,
-            grad: RefCell::new(0.0),
-            op: Some(Op::Add),
-            prev: vec![Rc::new(self), Rc::new(rhs)],
-        }
-    }
-}
-
-impl Mul for Tensor {
-    type Output = Self;
-
-    fn mul(self, rhs: Self) -> Self::Output {
-        let prod = self.data * rhs.data;
-
-        Self {
-            data: prod,
-            grad: RefCell::new(0.0),
-            op: Some(Op::Mul),
-            prev: vec![Rc::new(self), Rc::new(rhs)],
-        }
+pub fn print_graph(tensor: &Rc<Tensor>, depth: usize) {
+    let indent = " ".repeat(depth * 2);
+    println!("{}{}", indent, tensor);
+    for child in &tensor.prev {
+        print_graph(child, depth + 1);
     }
 }
--- micrograd/src/main.rs
+++ micrograd/src/main.rs
@@ -1,28 +1,21 @@
-use micrograd::engine;
+use micrograd::engine::{print_graph, Tensor};
 use std::rc::Rc;
 
 fn main() {
-    let compute = || -> engine::Tensor {
-        let x1 = engine::Tensor::new(2.0);
-        let x2 = engine::Tensor::new(0.0);
-
-        let w1 = engine::Tensor::new(-3.0);
-        let w2 = engine::Tensor::new(1.0);
-
-        let b = engine::Tensor::new(6.8813735870195432);
-
-        let x1w1 = x1 * w1;
-        let x2w2 = x2 * w2;
-        let x1w1x2w2 = x1w1 + x2w2;
-
-        let n = x1w1x2w2 + b;
-        n.tanh()
+    let compute = || -> Rc<Tensor> {
+        let x1 = Tensor::new(2.0, "x1");
+        let x2 = Tensor::new(0.0, "x2");
+        let w1 = Tensor::new(-3.0, "w1");
+        let w2 = Tensor::new(1.0, "w2");
+        let b = Tensor::new(6.8813735870195432, "b");
+        let x1w1 = Tensor::mul(&x1, &w1);
+        let x2w2 = Tensor::mul(&x2, &w2);
+        let x1w1x2w2 = Tensor::add(&x1w1, &x2w2);
+        let n = Tensor::add(&x1w1x2w2, &b);
+        Tensor::tanh(&n)
     };
 
     let output = compute();
-    *output.grad.borrow_mut() = 1.0;
-    output.backward();
-
-    println!("Output: {}", output);
-    println!("{}", output.prev_to_string());
+    Tensor::backward(&output);
+    print_graph(&output, 0);
 }
